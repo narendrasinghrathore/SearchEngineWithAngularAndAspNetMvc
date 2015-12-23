c9932102ee75da3a06bdbb4e73d9f588044a292f
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2015-12-23 08:40:04 +0530

    My new changes

---
+++ SearchEngine.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SearchEngine.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SearchEngine.API/WebServices/SearchService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="SearchService.asmx.cs" Class="SearchEngine.API.WebServices.SearchService" %>
